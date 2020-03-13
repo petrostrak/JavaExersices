3ce557c1efe3a692e235fdc970cb6240403af380
petros <45797153+petrostrak@users.noreply.github.com>
2020-03-13 23:41:56 +0200

    Update WhatClass.java

--- w2d5/WhatClass.java
+++ w2d5/WhatClass.java
@@ -1,28 +1,28 @@
 //created with gedit 3.34.0
 public class WhatClass{
 	public static void main(String[] args){
-		int age = Integer.parseInt(args[0]);
+		int age = Integer.parseInt(args[0]);	//Convert String into Integer.
 		char a;
 		System.out.println(age);
 		if(age > 0){
 			if(age < 5 ){
 				a = 'A';
-//				System.out.println("You re still baby, no education for you yet.");		
+				//System.out.println("You re still baby, no education for you yet.");		
 			}else if(age < 12){
-//				System.out.println("You need to go to Elementary School.");
+				//System.out.println("You need to go to Elementary School.");
 				a = 'B';
 			}else if(age < 14){
-//				System.out.println("You need to go to Junior High School.");
+				//System.out.println("You need to go to Junior High School.");
 				a = 'C';
 			}else if(age < 18){
-//				System.out.println("You need to go to High School.");
+				//System.out.println("You need to go to High School.");
 				a = 'D';
 			}else{
-//				System.out.println("You need to go to College");
+				//System.out.println("You need to go to College");
 				a = 'E';			
 			}
 		}else
-//			System.out.println("Give me a positive number.");
+			//System.out.println("Give me a positive number.");
 			a = 'F';
 
 		switch(a){
